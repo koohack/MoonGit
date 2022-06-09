29f44038b881bc40d88c256e64e4aa95b7b305ad
koohack <qkrpsh97@naver.com>
2022-06-09 20:46:22 +0900

    login test

---
+++ backend/test/testCode.py
@@ -0,0 +1,29 @@
+## ----------------------------
+## Login test code
+## ----------------------------
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+import pymysql
+import pandas as pd
+
+admin_name = "root"
+admin_pwd = "qwer1234"
+db_host = "localhost"
+db_name = "moongit_login"
+
+conn = pymysql.connect(host=db_host, user=admin_name, password=admin_pwd, db=db_name, charset="utf8")
+cursor = conn.cursor()
+
+sql = "select * from userList where id='user6'"
+
+temp = conn.cursor()
+temp.execute(sql)
+result = temp.fetchall()
+print(len(result))
+for i in result:
+    print(i)
+
+
+
+conn.close()
